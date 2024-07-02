b9b2528499f50dbc8323de8929e1897b8880d100
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-07-01 22:23:38 -0500

    got line number implemented

--- scanner.hs
+++ scanner.hs
@@ -44,10 +44,10 @@ data TokenType
   | EOF
   deriving (Show, Eq)
 
-addToken :: TokenType -> [Token] -> [Token]
-addToken tokenType tokens = (t : tokens)
+addToken :: TokenType -> [Token] -> Int -> [Token]
+addToken tokenType tokens line = (t : tokens)
   where
-    t = Token tokenType "a" "a" 3
+    t = Token tokenType "a" "a" line
 
 data Token = Token {tokenType :: TokenType, lexeme :: String, literal :: String, line :: Int}
 
@@ -55,7 +55,15 @@ instance Show Token where -- Todo, add tokenType to show
   show (Token tokenType lexeme literal _) = show tokenType
 
 scan :: String -> [Token]
-scan content = reverse $ foldl (\acc line -> get line acc) [] (lines content)
+scan content =
+  reverse $
+    fst $
+      foldl
+        ( \(tokens, lineNumber) line ->
+            (get line tokens lineNumber, lineNumber + 1)
+        )
+        ([], 0)
+        (lines content)
 
 quoteLookAhead :: String -> String -> (String, String)
 quoteLookAhead [] _ = error "Quotes not terminated"
@@ -63,14 +71,14 @@ quoteLookAhead ('\\' : '"' : xs) acc = quoteLookAhead xs (acc ++ "\\\"")
 quoteLookAhead ('"' : xs) acc = (acc, xs)
 quoteLookAhead (x : xs) acc = quoteLookAhead xs (acc ++ xs)
 
-get :: String -> [Token] -> [Token]
-get [] tokens = tokens
-get ('"' : xs) tokens = get rest (addToken STRING tokens)
+get :: String -> [Token] -> Int -> [Token]
+get [] tokens l = tokens
+get ('"' : xs) tokens l = get rest (addToken STRING tokens l) l
   where
     (literal, rest) = quoteLookAhead xs []
-get ('=' : '=' : xs) tokens = get xs (addToken EQUAL_EQUAL tokens)
-get ('(' : xs) tokens = get xs (addToken LEFT_PAREN tokens)
-get (_ : xs) tokens = get xs tokens
+get ('=' : '=' : xs) tokens l = get xs (addToken EQUAL_EQUAL tokens l) l
+get ('(' : xs) tokens l = get xs (addToken LEFT_PAREN tokens l) l
+get (_ : xs) tokens l = get xs tokens l
 
 main :: IO ()
 main = do
