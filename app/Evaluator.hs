1d3d10c24cdd1a81bea969ea3a2d110404d23c90
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-07-08 13:21:58 -0500

    global env finished

--- app/Evaluator.hs
+++ app/Evaluator.hs
@@ -6,54 +6,58 @@ import Shared
 
 type Env = Map.Map String Value
 
-evaluateExpression :: Expression -> Value
-evaluateExpression expression = case expression of
-  (Grouping e) -> evaluateExpression e
+evaluateExpression :: Expression -> Env -> Value
+evaluateExpression expression env = case expression of
+  (Grouping e) -> evaluateExpression e env
   -- Number
-  (Negate e) -> case evaluateExpression e of
+  (Negate e) -> case evaluateExpression e env of
     IntVal a -> IntVal (-a)
     BoolVal a -> BoolVal (not a)
     _ -> error "Can only negate a number"
   (Number e) -> IntVal e
   (Boolean e) -> BoolVal e
   (Str e) -> StringVal e
-  (Multiply x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Identifier name) -> case Map.lookup name env of
+    Just v -> v
+    _ -> error "Can't find"
+  (Multiply x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> IntVal (rx * ry)
     _ -> error "Bad"
-  (Divide x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Divide x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal 0) -> error "Can't divide by 0"
     (IntVal rx, IntVal ry) -> IntVal (rx `div` ry)
     _ -> error "Bad"
-  (Add x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Add x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> IntVal (rx + ry)
     _ -> error "Bad"
-  (Exponent x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Exponent x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> IntVal (rx ^ ry)
     _ -> error "Bad"
-  (Subtract x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Subtract x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> IntVal (rx - ry)
     _ -> error "Bad"
   -- Comparison
-  (Not x) -> case evaluateExpression x of
+  (Not x) -> case evaluateExpression x env of
     BoolVal y -> BoolVal (not y)
-  (Greater x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Greater x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx > ry)
     _ -> error "Bad"
-  (GreaterEqual x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (GreaterEqual x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx >= ry)
-  (Less x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Less x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx < ry)
     _ -> error "Bad"
-  (LessEqual x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (LessEqual x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx <= ry)
     _ -> error "Bad"
-  (Equal x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (Equal x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx == ry)
     (BoolVal rx, BoolVal ry) -> BoolVal (rx == ry)
     _ -> error "Bad"
-  (NotEqual x y) -> case (evaluateExpression x, evaluateExpression y) of
+  (NotEqual x y) -> case (evaluateExpression x env, evaluateExpression y env) of
     (IntVal rx, IntVal ry) -> BoolVal (rx /= ry)
     (BoolVal rx, BoolVal ry) -> BoolVal (rx /= ry)
+  _ -> error "bad"
 
 evaluateDeclaration :: Declaration -> Env -> (IO (), Env)
 evaluateDeclaration expr env =
@@ -62,8 +66,8 @@ evaluateDeclaration expr env =
       (ExpressionStatement expr) -> do
         let _ = evaluateExpression expr
         (return (), env)
-      (PrintStatement expr) -> (print ("LOG", evaluateExpression expr), env)
-    (VarDeclaration name expr) -> (return (), Map.insert name (evaluateExpression expr) env)
+      (PrintStatement expr) -> (print ("LOG", evaluateExpression expr env), env)
+    (VarDeclaration name expr) -> (return (), Map.insert name (evaluateExpression expr env) env)
 
 eval :: [Declaration] -> Env -> IO ()
 eval declarations env = foldM_ processDeclaration env declarations
